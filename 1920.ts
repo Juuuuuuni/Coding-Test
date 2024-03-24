4df4875ca28aff18ca09054eb0ea3f6152a6ec0f
juneheein <june.programmer@gmail.com>
2024-03-24 23:46:36 +0900

    feat: 1920 문제 풀이

--- 1920.ts
+++ 1920.ts
@@ -1,8 +1,10 @@
 /*
 * https://www.acmicpc.net/problem/2805
 * 절단기 높이의 최소값을 구해야한다.
-* 이진 탐색으로 중간 값마다 계산해본다.
+* 절단기 높이를 이진 탐색으로 돌면서 중간 값마다 나무를 잘라서 계산해본다.
 *
+* 시간복잡도 : O(NlogN)
+* 공간복잡도 : O(N)
 * */
 
 
@@ -16,28 +18,36 @@ const [NM, trees] = [
 ];
 const [N, M] = NM.split(" ").map(Number);
 const treeNumbers = trees.split(" ").map(Number);
-const heights = new Array(Math.max(...treeNumbers)).fill(0).map((t, index) => index + 1);
 
 let lt = 0;
-let rt = heights.length - 1;
+let rt = Math.max(...treeNumbers);
 let BestH = 0;
 
 while (lt <= rt) {
     const mid = Math.floor((lt + rt) / 2);
-    const midChoppingHeight = heights[mid];
 
     let choppedTreeHeight = 0;
-    treeNumbers.map((tree) => {
-        const temp  = tree - midChoppingHeight;
-        if (temp > 0 ) choppedTreeHeight += temp;
 
-    });
+    /*
+    * for문으로 돌릴때와 map으로 돌릴때 메모리 차이가 많이 남
+    * for문으로 처리했을 때 : 131352KB	716ms
+    * map으로 처리했을 때 : 369956KB	1336ms
+    * */
+    for (let x of treeNumbers) {
+        if (x > mid) {
+            choppedTreeHeight += x - mid;
+        }
+    }
+    // treeNumbers.map((tree) => {
+    //     const temp  = tree - mid;
+    //     if (temp > 0 ) choppedTreeHeight += temp;
+    // });
 
     if (choppedTreeHeight < M) {
         rt = mid - 1;
     }
     else if (choppedTreeHeight >= M) {
-        if (BestH < midChoppingHeight) BestH = midChoppingHeight;
+        if (BestH < mid) BestH = mid;
         lt = mid + 1;
     }
 }
