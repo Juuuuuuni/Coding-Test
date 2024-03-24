7c7444d958685879333a88075d727ae6f2ecadbb
juneheein <june.programmer@gmail.com>
2024-03-24 09:58:28 +0900

    feat: 11053 BS 풀이 추가

---
+++ 11053_binary-search.ts
@@ -0,0 +1,29 @@
+/*
+* https://www.acmicpc.net/problem/11053
+LIS: Longest Increasing Subsequence
+*
+* Binary Search를 이용
+* 시간복잡도: O(NlogN)
+* 공간복잡도: O(N)
+* https://velog.io/@arthur/12738.-%EA%B0%80%EC%9E%A5-%EA%B8%B4-%EC%A6%9D%EA%B0%80%ED%95%98%EB%8A%94-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4-3-node.js-javascript
+*
+* */
+import { binarySearch } from "./utils/binarySeach";
+
+const fs = require('fs');
+const [sequenceLength, sequenceString] = fs.readFileSync('/dev/stdin').toString().split('\n');
+// const [sequenceLength, sequenceString] = ["6", "10 20 10 30 20 50"]
+const sequenceList = sequenceString.split(" ").map(Number);
+
+const LIS = [sequenceList[0]];
+for (let num of sequenceList) {
+    if (LIS[LIS.length - 1] < num) {
+        LIS.push(num);
+    } else {
+        let idx = binarySearch(num, LIS);
+        LIS[idx] = num;
+    }
+}
+
+
+console.log(LIS.length);
---
+++ utils/binarySeach.ts
@@ -0,0 +1,14 @@
+export const binarySearch = (target: number, array: number[]) => {
+    let lt = 0;
+    let rt = array.length - 1;
+
+    while (lt <= rt) {
+        let mid = Math.floor((lt + rt) / 2);
+
+        if (array[mid] === target) return mid;
+        else if (array[mid] < target) lt = mid + 1;
+        else rt = mid - 1;
+    }
+
+    return lt;
+}
