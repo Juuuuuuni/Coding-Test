008df734d66a49f7f5a730d9191d131836eeaf7e
Juuuuuuni <june.programmer@gmail.com>
2024-12-18 17:01:01 +0900

    2941 문제 풀이

---
+++ 2941/answer.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const inputs = fs.readFileSync("./input.txt").toString().trim();
+
+const croAlphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+
+let modifiedInputs = inputs;
+croAlphabet.forEach(alpha => modifiedInputs = modifiedInputs.split(alpha).join("*"));
+
+console.log(modifiedInputs.length);
