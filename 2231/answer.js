f628312b48e50fb8f115ee4fe1e3ad54a963cc6b
Juuuuuuni <june.programmer@gmail.com>
2024-12-19 14:02:17 +0900

    2231, 2798 문제풀이

---
+++ 2231/answer.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const target = fs.readFileSync("./input.txt").toString();
+
+const numericTarget = Number(target);
+let min = 0;
+let i = numericTarget - Number(target.length) * 9;
+
+
+while (i < numericTarget) {
+  let result = i;
+  for (const element of String(i)) {
+    result += Number(element);
+  }
+
+  if (numericTarget === result) {
+    min = i;
+    break;
+  }
+
+  i++;
+}
+
+console.log(min);
---
+++ 2798/answer.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const [NM, cards] = fs.readFileSync("./input.txt").toString().trim().split("\n").map(input => input.split(" ").map(Number));
+const [N, M] = NM;
+
+let max = 0
+for (let i = 0; i < N - 2; i++) {
+  for (let j = i + 1; j < N - 1; j++) {
+    for (let k = j + 1; k < N; k++) {
+      const sum = cards[i] + cards[j] + cards[k];
+
+      if (sum <= M && max < sum) max = sum;
+    }
+  }
+}
+
+console.log(max);
