bd5938c2b015a99296fe1ed54552d3b69b93073e
juneheein <june.programmer@gmail.com>
2024-12-27 23:51:37 +0900

    프로그래머스 176962 문제 리팩토링

--- Programmers/176962/answer.js
+++ Programmers/176962/answer.js
@@ -1,45 +1,58 @@
-console.log(solution([["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]]));
-console.log(solution([["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]));
+console.log(
+  solution([
+    ["korean", "11:40", "30"],
+    ["english", "12:10", "20"],
+    ["math", "12:30", "40"],
+  ]),
+);
+console.log(
+  solution([
+    ["science", "12:40", "50"],
+    ["music", "12:20", "40"],
+    ["history", "14:00", "30"],
+    ["computer", "12:30", "100"],
+  ]),
+);
 
 function solution(plans) {
   var answer = [];
 
-  // start값으로 오름차순 정렬
-  plans.sort((a, b) => {
-    const aStartMinuteTime = convertToNumberMinute(a[1]);
-    const bStartMinuteTime = convertToNumberMinute(b[1]);
-
-    return aStartMinuteTime - bStartMinuteTime;
-  });
+  const sortedPlans =   plans
+    .map(([name, start, playTime]) => [
+      name,
+      convertToNumberMinute(start),
+      Number(playTime),
+    ]).sort((a, b) => a[1] - b[1]);
 
   const delayedHomeworks = [];
-  while (plans.length) {
-    const currentHomework = plans.shift();
+
+  while (sortedPlans.length) {
+    const currentHomework = sortedPlans.shift();
     const [name, start, playTime] = currentHomework;
 
-    if (plans.length === 0) {
-      console.log("3. name: ", name);
+    if (sortedPlans.length === 0) {
       answer.push(name);
       break;
     }
-    const [nextName, nextStart, nextPlayTime] = plans[0];
-    const startMinuteTime = convertToNumberMinute(start);
-    const nextStartMinuteTime = convertToNumberMinute(nextStart);
+    const [_, nextStart] = sortedPlans[0];
 
-    const diff = nextStartMinuteTime - startMinuteTime;
+    const diff = nextStart - start;
 
-    if (diff < Number(playTime)) delayedHomeworks.push([name, Number(playTime) - diff]);
+    if (diff < playTime)
+      delayedHomeworks.push([name, playTime - diff]);
     else {
       answer.push(name);
-      let restTime = diff - Number(playTime);
+      let restTime = diff - playTime;
+
       while (restTime > 0) {
         if (delayedHomeworks.length === 0) break;
+
         const [name, restPlayTime] = delayedHomeworks.pop();
+
         if (restTime < restPlayTime) {
           delayedHomeworks.push([name, restPlayTime - restTime]);
           restTime = 0;
-        }
-        else {
+        } else {
           restTime = restTime - restPlayTime;
           answer.push(name);
         }
@@ -47,13 +60,13 @@ function solution(plans) {
     }
   }
 
-  delayedHomeworks.reverse().forEach(homework => answer.push(homework[0]));
-
+  delayedHomeworks.reverse().forEach((homework) => answer.push(homework[0]));
 
   return answer;
 }
 
 function convertToNumberMinute(stringTime) {
-  const [hour, minute] = stringTime.split(":");
-  return Number(hour) * 60 + Number(minute);
+  const [hour, minute] = stringTime.split(":").map(Number);
+  return hour * 60 + minute;
 }
+
